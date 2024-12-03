1a7b1a22af18a25442b7c40863b910cf575e5e38
Devshree0704 <devshrees123@gmail.com>
2024-12-03 08:44:33 +0530

    image-carousel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState} from 'react'
 
 function App() {
+  const [images, setImages] = useState([])
+  const [id, setId] = useState(0)
+
+  useEffect(() => {
+    async function getRecipes(){
+      try{
+        const getData = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s')
+        const response = await getData.json()
+        setImages(response.meals.map((element) => element.strMealThumb))
+      }catch(err){
+        throw new Error('Something is wrong!!')
+      }
+    }
+    getRecipes()
+  }, []) 
+
+  const totalImages = images.length
+
+  const handleNext = () => {
+    if(id<totalImages-1) setId((id) => id+1)
+    else setId(0)
+  }
+
+  const handlePrev = () => {
+    if(id>0) setId((id) => id-1)
+  }
+
+  console.log('images:', images)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Image Carousel</h3>
+      <img src={images[id]} alt='image'/>
+      <div className='actions'>
+        <button className='prev' disabled={id === 0} onClick={handlePrev}>Prev</button>
+        {id}
+        <button className='next' disabled={id === totalImages-1} onClick={handleNext}>Next</button>
+      </div>
     </div>
   );
 }
